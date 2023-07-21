2eb0c2139f5702bb5177ffcf4ccf9d63b8fe720a
yeom-c <yjc720@gmail.com>
2023-07-21 14:22:59 +0900

    feat: local strategy, guard, jwt 인증

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 import { UsersModule } from './users/users.module';
 import { LoggerModule } from '@app/common';
-import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
   imports: [
@@ -18,7 +21,17 @@ import * as Joi from 'joi';
         JWT_EXPIRATION_SEC: Joi.number().default(3600),
       }),
     }),
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: `${configService.get<string>('JWT_EXPIRATION_SEC')}s`,
+        },
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AuthController],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
---
+++ apps/auth/src/strategies/local.strategy.ts
@@ -0,0 +1,19 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { UsersService } from '../users/users.service';
+import { Strategy } from 'passport-local';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly userService: UsersService) {
+    super({ usernameField: 'email' });
+  }
+
+  async validate(email: string, password: string) {
+    try {
+      return this.userService.verifyUser(email, password);
+    } catch (err) {
+      throw new UnauthorizedException(err);
+    }
+  }
+}
