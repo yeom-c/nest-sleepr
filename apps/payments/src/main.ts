fd552a203b9c88c03fbddb17cc471415bd721df4
yeom-c <yjc720@gmail.com>
2023-07-25 11:38:50 +0900

    feat: payments app with stripe

---
+++ apps/payments/src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { PaymentsModule } from './payments.module';
+import { Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from 'nestjs-pino';
+
+async function bootstrap() {
+  const app = await NestFactory.create(PaymentsModule);
+  const configServie = app.get(ConfigService);
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: { host: '0.0.0.0', port: configServie.get('TCP_PORT') },
+  });
+  app.useLogger(app.get(Logger));
+  await app.startAllMicroservices();
+}
+bootstrap();
